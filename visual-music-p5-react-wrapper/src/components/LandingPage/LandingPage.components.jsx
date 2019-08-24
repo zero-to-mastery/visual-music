6841d696c419e21bb1c6c92a27aa559fbb490982
Operel <oriperelman@gmail.com>
2019-08-24 14:59:42 +0300

    Initial commit: set up jsx and css file for landing page in /src/component/LandingPage Install react-spring dependency

---
+++ visual-music-p5-react-wrapper/src/components/LandingPage/LandingPage.components.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './LandingPage.styles.css';
+
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
+
+class LandingPage extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h2>Working</h2>
+    )
+  }
+}
+
+export default LandingPage;
