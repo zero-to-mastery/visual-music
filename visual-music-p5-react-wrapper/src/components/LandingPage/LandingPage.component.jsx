2d30ca314500441ac18771bcbd40fe2c2efb9ba8
Operel <oriperelman@gmail.com>
2019-08-25 21:16:03 +0300

    add first section of landing page and half of second section

--- visual-music-p5-react-wrapper/src/components/LandingPage/LandingPage.component.jsx
+++ visual-music-p5-react-wrapper/src/components/LandingPage/LandingPage.component.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './LandingPage.styles.css';
-import logo from '../../assets/LogoSVG.svg'
+import logo from '../../assets/LogoSVG.svg';
 
-// import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
+import { config } from 'react-spring/renderprops'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+
+import Button from '../Button/Button.component.jsx';
 
 class LandingPage extends Component {
   constructor() {
@@ -12,12 +15,47 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <div className="landing-page-wrap">
-        <nav><img src={logo} alt="logo" height="100px" width="100px" /></nav>
-        <div>
-          <h2>Working</h2>
-        </div>
-      </div>      
+      <>
+        <div className="landing-page-bg" />
+        <nav>
+          <img src={logo} alt="logo" height="100px" width="100px" />
+        </nav>
+        <Parallax pages={3} config={{ tension: 70, friction: 40 }}>
+          <ParallaxLayer offset={1} speed={0.9} style={{ backgroundColor: 'rgba(13, 78, 103, 0.56)' }}/>
+          <ParallaxLayer offset={2} speed={0.55} style={{ backgroundColor: 'rgba(41, 172, 109, 0.56)' }}/>
+
+          <section id="section-one">
+            <ParallaxLayer speed={0.2} offset={0.35}>
+              <h2 className="content-wrapper heading">Discover and Share Music Visualization</h2>
+            </ParallaxLayer>
+            <ParallaxLayer speed={0.09} offset={0.58}>
+              <hr style={{ marginLeft: '8%', width: '30%', border: '2px solid white' }} />
+            </ParallaxLayer>
+            <ParallaxLayer speed={0.03} offset={0.6}>
+              <p className="content-wrapper tagline">
+                An app that converts your favoriet music pieces into visual expresions.
+              </p>
+            </ParallaxLayer>
+            <ParallaxLayer speed={-0.12} offset={0.75}>
+              <Button text="Get Started" btnClass="getStarted" />
+            </ParallaxLayer>
+          </section>
+
+          <section id="section-two">
+          <ParallaxLayer speed={0.12} offset={1.32}>
+              <h2 className="content-wrapper heading">Best Experience</h2>
+            </ParallaxLayer>
+            <ParallaxLayer speed={0.08} offset={1.45}>
+              <hr style={{ marginLeft: '8%', width: '30%', border: '2px solid white' }} />
+            </ParallaxLayer>
+            <ParallaxLayer speed={0.01} offset={1.5}>
+              <p className="content-wrapper tagline">
+                Collection of visual themes to transcribe the emotional and rythmic effects of an uoloaded mp3 song.
+              </p>
+            </ParallaxLayer>\
+          </section>
+        </Parallax>
+      </>
     )
   }
 }
