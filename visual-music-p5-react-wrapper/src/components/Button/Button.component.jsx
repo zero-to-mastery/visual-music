bad77ae43326cab35eb2378083905bc2f886002a
Operel <oriperelman@gmail.com>
2019-08-27 16:52:46 +0300

    fix Button component issues: clicking when no handleRoute func is set diable css outline on focus in App.css

--- visual-music-p5-react-wrapper/src/components/Button/Button.component.jsx
+++ visual-music-p5-react-wrapper/src/components/Button/Button.component.jsx
@@ -12,7 +12,7 @@ import React from 'react';
 import './Button.styles.css';
 
 const Button = ({ text, btnClass, handleRouteChange, to }) => (
-  <button className={btnClass} onClick={() => handleRouteChange(to)}>
+  <button className={btnClass} onClick={handleRouteChange ? () => handleRouteChange(to) : null}>
     {text}
   </button>
 );
