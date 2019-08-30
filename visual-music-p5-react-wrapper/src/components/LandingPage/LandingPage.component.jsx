a657464d9b19207086046421296d89933c2db787
Operel <oriperelman@gmail.com>
2019-08-30 15:38:02 +0300

    Add a scrolling button

--- visual-music-p5-react-wrapper/src/components/LandingPage/LandingPage.component.jsx
+++ visual-music-p5-react-wrapper/src/components/LandingPage/LandingPage.component.jsx
@@ -6,6 +6,7 @@ import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 
 import SectionOne from './sections/SectionOne.component.jsx';
 import SectionTwo from './sections/SectionTwo.component.jsx';
+import SectionThree from './sections/SectionThree/SectionThree.component.jsx';
 
 class LandingPage extends Component {
   constructor(props) {
@@ -18,13 +19,21 @@ class LandingPage extends Component {
       <>
         <div className="landing-page-bg" />
         <TopNav />
-        <Parallax pages={3} config={{ tension: 100, friction: 50 }}>
+        <Parallax
+          pages={3}
+          config={{ tension: 100, friction: 60 }}
+          ref={ref => (this.parallax = ref)}
+        >
 
           <ParallaxLayer offset={1} speed={0.9} style={{ backgroundColor: 'rgba(13, 78, 103, 0.56)' }} />
           <ParallaxLayer offset={2} speed={0.55} style={{ backgroundColor: 'rgba(41, 172, 109, 0.56)' }} />
 
-          <SectionOne handleRouteChange={this.props.handleRouteChange}/>
-          <SectionTwo />
+          <SectionOne
+            handleRouteChange={this.props.handleRouteChange}
+            scrollClick={() => this.parallax.scrollTo(1)}
+          />
+          <SectionTwo scrollClick={() => this.parallax.scrollTo(2)} />
+          <SectionThree scrollClick={() => this.parallax.scrollTo(0)} />
 
         </Parallax>
       </>
--- visual-music-p5-react-wrapper/src/components/LandingPage/sections/SectionTwo.component.jsx
+++ visual-music-p5-react-wrapper/src/components/LandingPage/sections/SectionTwo.component.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { ParallaxLayer } from 'react-spring/renderprops-addons';
 
 import Icons from './SectionTwoIcons/SectionTwoIcons.component.jsx'
+import Button from '../../Button/Button.component.jsx';
 
-const SectionTwo = () => (
+const SectionTwo = ({ scrollClick }) => (
   <section>
-    <ParallaxLayer speed={0.09} offset={1.22}>
+    <ParallaxLayer speed={0.09} offset={1.29}>
       <h2 className="content-wrapper heading">Best Experience</h2>
     </ParallaxLayer>
-    <ParallaxLayer speed={0.04} offset={1.35}>
+    <ParallaxLayer speed={0.04} offset={1.4}>
       <hr style={{ marginLeft: '8%', width: '30%', border: '2px solid white' }} />
     </ParallaxLayer>
-    <ParallaxLayer speed={0.005} offset={1.4}>
+    <ParallaxLayer speed={0.005} offset={1.42}>
       <p className="content-wrapper tagline">
         Collection of visual themes to transcribe the emotional and rythmic effects of an uoloaded mp3 song.
       </p>
     </ParallaxLayer>
     <Icons />
+    <ParallaxLayer speed={0} offset={1.75} style={{zIndex: '80'}}>
+      <Button text="Scroll" handleRouteChange={scrollClick} btnClass="scroll" />
+    </ParallaxLayer>
   </section>
 )
 
