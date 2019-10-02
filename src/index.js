35a47c7fcbaf3185b305d10771558d2ba9e1f1b1
unknown <kohli6010@gmail.com>
2019-10-02 16:35:55 +0530

    Added Login folder and signin component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './globalScss/index.scss';
-import App from './pages/App/App';
+// import App from './pages/App/App';
+import SignUp from '../src/pages/Login/Signup.components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SignUp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
