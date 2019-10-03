f5beeb19b995e356a317b18a2696105c67c17684
Ryan Boris <ryan.boris@gmail.com>
2019-10-03 16:29:46 -0400

    Wrap top-level App component with Router HOC

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './globalScss/index.scss';
 import App from './pages/App/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router } from 'react-router-dom';
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
