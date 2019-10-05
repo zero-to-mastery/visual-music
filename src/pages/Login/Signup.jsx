e2b59e86b78256d73aef9212fe329ca9d4a319d0
kohli6010 <kohli6010@gmail.com>
2019-10-05 14:42:31 +0530

    Done improvement according to given instruction

--- src/pages/Login/Signup.jsx
+++ src/pages/Login/Signup.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
-import './Signup.styles.scss';
+import classes from './Signup.module.scss';
+
+/*
+ Main component for Signup.
+
+ Allows users to input name, email address and password and click
+ signup button to submit the form
+
+ Current features:
+ 1. Input user name
+ 2. Input user email address
+ 3. Input user password
+ 4. Do not allow email address and password filed to be blank
+
+ TODO: - Action when form submit
+
+*/
 
 class SignUp extends React.Component {
     constructor() {
@@ -8,20 +24,16 @@ class SignUp extends React.Component {
             name: '',
             email: '',
             password: '',
-            type: 'password',
+            isHidden: true,
         };
     }
 
-    showHide = (e) => {
-        e.stopPropagation();
-        if (this.state.type === 'password') {
-            this.setState({ type: 'text' })
-        } else { 
-            this.setState({type: 'password'})
-        }
+    showHide = () => {
+        this.setState({ isHidden: !this.state.isHidden });
     };
 
-    handleSubmit = () => {
+    handleSubmit = (e) => {
+        e.preventDefault();
         console.log(this.state);
     };
 
@@ -32,34 +44,42 @@ class SignUp extends React.Component {
 
     render() {
         return (
-            <div className='signup-component'>
-                <div className='signup'>
-                    <div className='headline'>
-                        <div className='headline1'>Welcome to Visual Music!</div>
-                        <div className='sub-headline'>Get Started Absolutely free</div>
-                    </div>
-                    <div className='searchfields'>
-                        <div className='name'>
-                            <label htmlFor='name'>Name</label>
-                            <input type='text' name='name' id='name' required onChange={this.handleChange} />
+            <div className={classes.signupcomponent}>
+                <div className={classes.container}>
+                    <div className={classes.signup}>
+                        <div className={classes.headlines}>
+                            <h2 className={classes.headline}>Welcome to Visual Music!</h2>
+                            <span className={classes.subheadline}>Get Started Absolutely free</span>
                         </div>
-                        <div className='email'>
-                            <label htmlFor='Email'>Email Address</label>
-                            <input type='email' name='email' id='email' required onChange={this.handleChange} />
+                        <div className={classes.searchfields}>
+                            <div className={classes.name}>
+                                <label htmlFor='name'>Name</label>
+                                <input type='text' name='name' id='name' required onChange={this.handleChange} />
+                            </div>
+                            <div className={classes.email}>
+                                <label htmlFor='Email'>Email Address</label>
+                                <input type='email' name='email' id='email' required onChange={this.handleChange} />
+                            </div>
+                            <div className={classes.password}>
+                                <label htmlFor='password'>Password</label>
+                                <input
+                                    type={this.state.isHidden ? 'password' : 'text'}
+                                    name='password'
+                                    id='password'
+                                    required
+                                    onChange={this.handleChange}
+                                />
+                            </div>
                         </div>
-                        <div className='password'>
-                            <label htmlFor='password'>Password</label>
-                            <input type={this.state.type} name='password' id='password' onChange={this.handleChange} />
+                        <div className={classes.showpassword}>
+                            <input type='checkbox' name='showpassword' id='showpassword' onClick={this.showHide} />
+                            <label htmlFor='showpassword'>Show Password</label>
                         </div>
-                    </div>
-                    <div className='showpassword'>
-                        <input type='checkbox' name='showpassword' id='showpassword' onClick={this.showHide} />
-                        <label htmlFor='showpassword'>Show Password</label>
-                    </div>
-                    <div className='custom-button'>
-                        <button type='submit' onClick={this.handleSubmit}>
-                            Sign Up
-                        </button>
+                        <div className={classes.custombutton}>
+                            <button type='submit' onClick={this.handleSubmit}>
+                                Sign Up
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
