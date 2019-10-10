3f0cee6ceccd41b5812dfc24e372349a9891658d
YogiHa <hagaiharari1@gmail.com>
2019-10-10 17:32:51 +0300

    resolving-converstions

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -18,30 +18,17 @@ import logo from '../../assets/LogoSVG.svg';
 import Button from '../Button/Button';
 
 const TopNav = () => (
-	<div>
-		<nav className={classes.topNav}>
-			<img src={logo} alt="logo" height="85px" width="85px" />
-			<div>
-				<Link to="/login">
-					<Button text="Log In" btnClass="logIn" />
-				</Link>
-				<Link to="/register">
-					<Button text="Sign Up" btnClass="signUp" />
-				</Link>
-			</div>
-		</nav>
-		<nav className={classes.topNav}>
-			<img src={logo} alt="logo" height="85px" width="85px" />
-			<div>
-				<Link to="/login">
-					<Button text="Log In" btnClass="logIn" />
-				</Link>
-				<Link to={'/register'}>
-					<Button text="Sign Up" btnClass="signUp" />
-				</Link>
-			</div>
-		</nav>
-	</div>
+	<nav className={classes.topNav}>
+		<img src={logo} alt="logo" height="85px" width="85px" />
+		<div>
+			<Link to="/login">
+				<Button text="Log In" btnClass="logIn" />
+			</Link>
+			<Link to="/register">
+				<Button text="Sign Up" btnClass="signUp" />
+			</Link>
+		</div>
+	</nav>
 );
 
 export default TopNav;
