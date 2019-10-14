ce08e470127864d4377e17e70eff5de3c2066118
Nelson <tomneo2004@gmail.com>
2019-10-14 12:19:27 +0800

    Add comment, update VisualPanel onItemClick to have index value for callback function. Move VisualItem into components from VisualPanel folder. Make TestImages for VisualPanel together in one group

--- src/components/VisualPanel/VisualPanel.js
+++ src/components/VisualPanel/VisualPanel.js
@@ -3,17 +3,20 @@
 
     Props:
     - visualList: Type of array. An array contain all visualization data that need to be displayed
-    - onItemClick: Type of function. A callback function, 
-    which take 1 argument when an item is being clicked. Argument which mapped to specific
-    data in visual data array that was provided.
+    - onItemClick: Type of function. A callback function.
 
+    Callback:
+    - onItemClick: If provided in props. Function take 2 argument and is called 
+    when an item is being clicked. 
+        1.first argument: index of data in data array which was provided
+        2.second argument: data in data array which was provided
  
     Current features:
     - Display visulization list
 
 ************************************************************/
 import React from 'react';
-import VisualItem from './VisualItem/VisualItem';
+import VisualItem from '../VisualItem/VisualItem';
 import classes from './VisualPanel.module.scss';
 
 //data for testing purpose
@@ -21,59 +24,60 @@ const listData = [
     {
         id:1,
         title:'visual1',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg1.png')
     },
     {
         id:2,
         title:'visual2',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg2.png')
     },
     {
         id:3,
         title:'visual3',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg3.png')
     },
     {
         id:4,
         title:'visual4',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg4.png')
     },
     {
         id:5,
         title:'visual5',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg1.png')
     },
     {
         id:6,
         title:'visual6',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg2.png')
     },
     {
         id:7,
         title:'visual7',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg3.png')
     },
     {
         id:8,
         title:'visual8',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg4.png')
     },
     {
         id:9,
         title:'visual9',
-        imageSrc:require('./ItemImg.png')
+        imageSrc:require('./TestImages/ItemImg1.png')
     },
 ];
 
 const VisualPanel = ({visualList, onItemClick})=>{
 
+    //for test only since no data are given at moment
     visualList = visualList?visualList:listData;
 
     const onVisualItemClick = (index)=>{
         const item = visualList[index];
 
         if(onItemClick){
-            onItemClick(item);
+            onItemClick(index, item);
         }
     }
 
--- src/components/Visualizer/Visualizer.component.jsx
+++ src/components/Visualizer/Visualizer.component.jsx
@@ -28,76 +28,10 @@ class Visualizer extends React.Component {
             canvasWidth:0,
             canvasHeight:0
         };
-
-        //create a reference and reference to
-        //<div className={classes.visualizer}></div> in jsx
-        //so we can get react node back 
-        // this.visualizerRef = React.createRef();
     }
-
-    componentDidMount(){
-
-        //we listen window resize event and call
-        // onWindowResize method
-        // window.addEventListener('resize', this.onWindowResize)
-        // this.onWindowResize();
-    }
-
-    componentWillUnmount(){
-
-        //we remove window resize event
-        // window.removeEventListener('resize', this.onWindowResize);
-    }
-
-    //calculation canvas size base on parent element
-    // getCanvasSize(){
-
-    //     //get style properties from parent 
-    //     const style = window.getComputedStyle(this.visualizerRef.current.parentElement, null);
-
-    //     //get this style properties from <div className={classes.visualizer}></div>
-    //     const thisStyle = window.getComputedStyle(this.visualizerRef.current, null);
-
-    //     //get padding, border and margin width for <div className={classes.visualizer}></div>
-    //     const thisPaddingWidth = parseFloat(thisStyle.getPropertyValue('padding-left')) + parseFloat(thisStyle.getPropertyValue('padding-right'));
-    //     const thisBorderWidth = parseFloat(thisStyle.getPropertyValue('border-left')) + parseFloat(thisStyle.getPropertyValue('border-left'));
-    //     const thisMarginWidth = parseFloat(thisStyle.getPropertyValue('margin-left')) + parseFloat(thisStyle.getPropertyValue('margin-right'));
-
-    //     //get padding, border and margin width  from parent 
-    //     const paddingWidth = parseFloat(style.getPropertyValue('padding-left')) + parseFloat(style.getPropertyValue('padding-right'));
-    //     const borderWidth = parseFloat(style.getPropertyValue('border-left')) + parseFloat(style.getPropertyValue('border-left'));
-    //     const marginWidth = parseFloat(style.getPropertyValue('margin-left')) + parseFloat(style.getPropertyValue('margin-right'));
-
-    //     //calcualte new canvas width
-    //     const newCanvasWidth = parseFloat(style.getPropertyValue('width')) - 
-    //     paddingWidth  - thisPaddingWidth -  
-    //     borderWidth  -  thisBorderWidth -
-    //     marginWidth - thisMarginWidth;
-
-    //     //get padding, border and margin height for <div className={classes.visualizer}></div>
-    //     const thisPaddingHeight = parseFloat(thisStyle.getPropertyValue('padding-top')) + parseFloat(thisStyle.getPropertyValue('padding-bottom'));
-    //     const thisBorderHeight = parseFloat(thisStyle.getPropertyValue('border-top')) + parseFloat(thisStyle.getPropertyValue('border-bottom'));
-    //     const thisMarginHeight = parseFloat(thisStyle.getPropertyValue('margin-top')) + parseFloat(thisStyle.getPropertyValue('margin-bottom'));
-
-    //     //get padding, border and margin height  from parent 
-    //     const paddingHeight = parseFloat(style.getPropertyValue('padding-top')) + parseFloat(style.getPropertyValue('padding-bottom'));
-    //     const borderHeight = parseFloat(style.getPropertyValue('border-top')) + parseFloat(style.getPropertyValue('border-bottom'));
-    //     const marginHeight = parseFloat(style.getPropertyValue('margin-top')) + parseFloat(style.getPropertyValue('margin-bottom'));
-
-    //     //calcualte new canvas height
-    //     const newCanvasHeight = parseFloat(style.getPropertyValue('height')) - 
-    //     paddingHeight  - thisPaddingHeight -  
-    //     borderHeight  -  thisBorderHeight -
-    //     marginHeight - thisMarginHeight;
-
-    //     return {
-    //         canvasWidth: parseInt(newCanvasWidth),
-    //         canvasHeight: parseInt(newCanvasHeight)
-    //     }
-    // }
 
     onResize = (content)=>{
-        const {width, height, left, top} = content.offset;
+        const {width, height, left, top} = content;
         const cWidth = width - left;
         const cHeight = height - top;
         this.setState({canvasWidth:cWidth, canvasHeight:cHeight});
@@ -111,7 +45,7 @@ class Visualizer extends React.Component {
             <Measure
             offset
             onResize={content=>{
-                this.onResize(content);
+                this.onResize(content.offset);
             }}
             >
                 {({measureRef}) => (
@@ -129,17 +63,6 @@ class Visualizer extends React.Component {
             </Measure>
 
         );
-
-        /*<div className={classes.visualizer} {ref={this.visualizerRef}}>
-                <P5Wrapper
-                    sketch={sketch}
-                    volume={volume}
-                    isPlaying={isPlaying}
-                    uploadedSong={uploadedSong}
-                    canvasWidth={canvasWidth}
-                    canvasHeight={canvasHeight}
-                />
-        </div>*/
     }
 }
 
