3a6f92baf8f104ab519e51fdde0e0beb15cae128
Nelson <tomneo2004@gmail.com>
2019-10-25 17:04:59 +0800

    Import assets and setup inital success screen

---
+++ src/components/Success/Success.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {ReactComponent as GuitarIcon} from '../../assets/SuccessScreenAssets/electric-guitar-success.svg';
+import {ReactComponent as BackIcon} from '../../assets/ErrorScreenAssets/go-back.svg';
+
+import classes from './Success.module.scss';
+
+const Success = () => {
+
+    return (
+        <div className={classes.overlay}>
+            
+        </div>
+    );
+}
+
+export default Success;
