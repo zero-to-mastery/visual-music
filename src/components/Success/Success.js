1bab5a3070de92978e0de2626226936007c8ab95
Nelson <tomneo2004@gmail.com>
2019-10-25 17:56:51 +0800

    Success screen complete

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {ReactComponent as GuitarIcon} from '../../assets/SuccessScreenAssets/electric-guitar-success.svg';
-import {ReactComponent as BackIcon} from '../../assets/ErrorScreenAssets/go-back.svg';
+import {ReactComponent as BackIcon} from '../../assets/SuccessScreenAssets/back-btn-Icon-success.svg';
 
 import classes from './Success.module.scss';
 
@@ -9,7 +9,15 @@ const Success = () => {
 
     return (
         <div className={classes.overlay}>
-            
+            <div className={classes.successOverlay}>
+                <div className={classes.gradient}></div>
+                <div className={classes.content}>
+                    <GuitarIcon className={classes.guitarIcon} />
+                    <h1 className={classes.title}>Success!</h1>
+                    <BackIcon className={classes.backIcon} />
+                </div>
+                
+            </div>
         </div>
     );
 }
