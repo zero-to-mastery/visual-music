a2136971e8025c4015b92acb216a2e3498ce91fe
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-29 12:43:39 -0400

    Executed npm run format script. Confirmed Forgot-password page loads successfully.

--- src/pages/App/AppBrowser.js
+++ src/pages/App/AppBrowser.js
@@ -9,23 +9,23 @@ import UploadSong from '../../components/UploadSong/UploadSong';
 // 2. create AuthError mockup for user try to reach this route without connecting
 
 const AuthError = () => {
-	return <div> {'you need to login for visit the full app'} </div>;
+    return <div> {'you need to login for visit the full app'} </div>;
 };
 
 function AppBrowser() {
-	// uid and song are gonna be the trigers for re-render the the content this route will have.
-	// in case uid and song are exists, user gonna transfer to the visualizer
-	const uid = useSelector(state => state.firebase.auth.uid);
-	const song = useSelector(state => state.song);
-	return (
-		<div>
-			{uid ? (
-				<div> {song ? <App song={song} /> : <UploadSong />} </div>
-			) : (
-				<AuthError />
-			)}
-		</div>
-	);
+    // uid and song are gonna be the trigers for re-render the the content this route will have.
+    // in case uid and song are exists, user gonna transfer to the visualizer
+    const uid = useSelector(state => state.firebase.auth.uid);
+    const song = useSelector(state => state.song);
+    return (
+        <div>
+            {uid ? (
+                <div> {song ? <App song={song} /> : <UploadSong />} </div>
+            ) : (
+                <AuthError />
+            )}
+        </div>
+    );
 }
 
 export default AppBrowser;
