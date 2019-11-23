4274e90095b98098f50af39c179f6aa6270d3d4e
Nelson <tomneo2004@gmail.com>
2019-11-23 10:55:30 +0800

    npm run format

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_apiKey,
-	authDomain: process.env.REACT_APP_authDomain,
-	databaseURL: process.env.REACT_APP_databaseURL,
-	projectId: process.env.REACT_APP_projectId,
-	storageBucket: process.env.REACT_APP_storageBucket,
-	messagingSenderId: process.env.REACT_APP_messagingSenderId,
-	appId: process.env.appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.appId
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -6,29 +6,24 @@ import { store } from './store/store';
 
 import AppRouter from './routes/AppRouter';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Prompt from './components/Prompt/Prompt';
+
 import './globalScss/index.scss';
 
 // this line for making .env vars accessible in the whole app, https://stackoverflow.com/questions/49579028/adding-an-env-file-to-react-project
 require('dotenv').config();
 
-// const ROUTER = (
-// 	<Provider store={store}>
-// 		<Router>
-// 			<AppRouter />
-// 		</Router>
-// 	</Provider>
-// );
+const ROUTER = (
+    <Provider store={store}>
+        <Router>
+            <AppRouter />
+        </Router>
+    </Provider>
+);
 
-// store.firebaseAuthIsReady.then(() => {
-// 	ReactDOM.render(ROUTER, document.getElementById('root'));
-// });
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(ROUTER, document.getElementById('root'));
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-ReactDOM.render(<Prompt 
-	title='MyTitle' 
-	message='my very long message here for display' 
-	onClosed={(e)=>console.log('prompt closed button clicked')}/>, 
-document.getElementById('root'));
