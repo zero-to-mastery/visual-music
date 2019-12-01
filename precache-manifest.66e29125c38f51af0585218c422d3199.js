bdfb1fb6ddd6d272e677b1e3f7ea76055de6b841
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-01 17:28:22 +0000

    Deploy zero-to-mastery/visual-music to github.com/zero-to-mastery/visual-music.git:gh-pages

---
+++ precache-manifest.66e29125c38f51af0585218c422d3199.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38441d16aa40078779ba993d3677c71f",
+    "url": "/visual-music/index.html"
+  },
+  {
+    "revision": "cd30fd1b01d30a026c51",
+    "url": "/visual-music/static/css/main.7c45d8cf.chunk.css"
+  },
+  {
+    "revision": "9fe55c58320bd0deea52",
+    "url": "/visual-music/static/js/2.38d60d51.chunk.js"
+  },
+  {
+    "revision": "cd30fd1b01d30a026c51",
+    "url": "/visual-music/static/js/main.958d645d.chunk.js"
+  },
+  {
+    "revision": "3cec1cce566137743d7f",
+    "url": "/visual-music/static/js/runtime~main.87fd8d08.js"
+  },
+  {
+    "revision": "3c4740d2479d4db2f3fd1ba23ee9e248",
+    "url": "/visual-music/static/media/ItemImg1.3c4740d2.png"
+  },
+  {
+    "revision": "827e7d5a776b801518e5120c0fa3cd39",
+    "url": "/visual-music/static/media/ItemImg2.827e7d5a.png"
+  },
+  {
+    "revision": "5e422a15fa1e73db204b016a867f0d6a",
+    "url": "/visual-music/static/media/ItemImg3.5e422a15.png"
+  },
+  {
+    "revision": "e82407656548290118a978f8593544a4",
+    "url": "/visual-music/static/media/ItemImg4.e8240765.png"
+  },
+  {
+    "revision": "36625760cebd9f17a9a4d9f067055189",
+    "url": "/visual-music/static/media/LoadingBars.36625760.svg"
+  },
+  {
+    "revision": "114db13f35d981f1d562b07a14f743a7",
+    "url": "/visual-music/static/media/LoadingCircle.114db13f.svg"
+  },
+  {
+    "revision": "a75b3a33956f5d02d0e210d1086abb2e",
+    "url": "/visual-music/static/media/LogoSVG.a75b3a33.svg"
+  },
+  {
+    "revision": "7aac30b3cfb7ac70dc2927d18e9e12b4",
+    "url": "/visual-music/static/media/Rolling.7aac30b3.svg"
+  },
+  {
+    "revision": "b927523591859d234259703a70c18903",
+    "url": "/visual-music/static/media/bg-image2.b9275235.gif"
+  },
+  {
+    "revision": "f1901f87022756b7bdcb312dd400b0db",
+    "url": "/visual-music/static/media/blackHairAndBubbles.f1901f87.png"
+  },
+  {
+    "revision": "8bbb1d47854e463a88b1a12cb2572b05",
+    "url": "/visual-music/static/media/download-icon.8bbb1d47.svg"
+  },
+  {
+    "revision": "b5ab3f373ba70cfddb4823a481900d6c",
+    "url": "/visual-music/static/media/download-ready-icon.b5ab3f37.svg"
+  },
+  {
+    "revision": "721a1f17706a33b462d85f0c7d38f7bb",
+    "url": "/visual-music/static/media/github.721a1f17.svg"
+  },
+  {
+    "revision": "9d9fa02e64ad019708e795d099a4c256",
+    "url": "/visual-music/static/media/linkedIn.9d9fa02e.svg"
+  },
+  {
+    "revision": "8c126d0a214ec8a6d2305087720e9538",
+    "url": "/visual-music/static/media/pause-icon.8c126d0a.svg"
+  },
+  {
+    "revision": "7f7c42ffbf72ebcc43bc03de9f95f2d1",
+    "url": "/visual-music/static/media/play-icon.7f7c42ff.svg"
+  },
+  {
+    "revision": "b0e1a4d58194713da9a0b15db9215b77",
+    "url": "/visual-music/static/media/snapshot-icon-white-dark.b0e1a4d5.svg"
+  },
+  {
+    "revision": "6a00f94d18ed8512290a62cef7278237",
+    "url": "/visual-music/static/media/song-icon.6a00f94d.svg"
+  },
+  {
+    "revision": "3ed12b70e928c41ff4937a7a096d17aa",
+    "url": "/visual-music/static/media/volume-icon.3ed12b70.svg"
+  }
+]);
